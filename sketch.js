8dcfca8f2613d1f312a48a5550c8cb727a22a65f
shikha1802 <135698911+shikha1802@users.noreply.github.com>
2023-07-04 19:25:20 +1000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,108 +1,40 @@
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
-
-var cloud, cloudsGroup, cloudImage;
-var obstcle1,obstcle2,obstcle3,obstcle4,obstcle5,obstcle6
-
-
-
-var newImage;
-
-function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  trex_collided = loadAnimation("trex_collided.png");
-  
-  groundImage = loadImage("ground2.png");
-  
-  cloudImage = loadImage("cloud.png");
- obstcle1 = loadImage("obstacle1.png")
- obstcle2 = loadImage("obstacle2.png")
- obstcle3 = loadImage("obstacle3.png")
- obstcle4 = loadImage("obstacle4.png")
- obstcle5 = loadImage("obstacle5.png")
- obstcle6 = loadImage("obstacle6.png")
-}
 
 function setup() {
-  createCanvas(600, 200);
+  createCanvas(400,400);
+  background(51);
+  box = createSprite(200,200,30,30);
 
-  trex = createSprite(50,160,20,50);
-  trex.addAnimation("running", trex_running);
-  // trex.addAnimation("collided",trex_collided)
-  trex.scale = 0.5;
-  
-  ground = createSprite(200,180,400,20);
-  ground.addImage("ground",groundImage);
-  ground.x = ground.width /2;
-  ground.velocityX = -4;
-  
-  invisibleGround = createSprite(200,190,400,10);
-  invisibleGround.visible = false;
-  
-  console.log("Hello"+ 5)
-  
 }
 
-function draw() {
-  background(180);
+function draw() 
+{
+
+  // write the code to change background color 
+  // to red when RIGHT_ARROW is pressed
   
   
-  if(keyDown("space")&& trex.y >= 100) {
-    trex.velocityY = -10;
+
+  if (keyIsDown(LEFT_ARROW)) 
+  {
+    background("blue");
+    
   }
-  
-  trex.velocityY = trex.velocityY + 0.8
-  
-  if (ground.x < 0){
-    ground.x = ground.width/2;
+ 
+    if (keyIsDown(UP_ARROW)) 
+  {
+    background("yellow");
+   
   }
+
+  if (keyIsDown(DOWN_ARROW)) 
+  {
+    background("green");
+  }
+
+if (keyIsDown(RIGHT_ARROW)) {
+  background("red")
+}
   
-  trex.collide(invisibleGround);
-  
-  //spawn the clouds
-  spawnClouds();
-  spawnObstcle ()
   drawSprites();
 }
 
-function spawnClouds() {
-  //write code here to spawn the clouds
-  if (frameCount % 60 === 0) {
-    cloud = createSprite(600,100,40,10);
-    cloud.addImage(cloudImage)
-    cloud.y = Math.round(random(10,60))
-    cloud.scale = 0.4;
-    cloud.velocityX = -3;
-    
-    
-    //assigning lifetime to the variable
-    cloud.lifetime = 134
-    
-    //adjust the depth
-    cloud.depth = trex.depth
-    trex.depth = trex.depth + 1;
-    }
-}
-
-function spawnObstcle () {
-  if (frameCount % 80 == 0) {
-    obstcle = createSprite (600,160,20,20)
-    obstcle.velocityX = -3
-    var r = Math.round (random(1,6))
-    switch (r) {
-      case 1:obstcle.addImage (obstcle1)
-      break;
-      case 2:obstcle.addImage (obstcle2)
-      break;
-      case 3:obstcle.addImage (obstcle3)
-      break;
-      case 4:obstcle.addImage (obstcle4)
-      break;
-      case 5:obstcle.addImage (obstcle5)
-      break;
-      case 6:obstcle.addImage (obstcle6)
-      default:break
-    }
-    obstcle.scale = 0.6
-  }
-}
